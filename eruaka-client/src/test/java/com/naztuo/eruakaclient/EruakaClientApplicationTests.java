2282fc3b977a0063d0a14c27e762a34e96b3aa5b
naztuo <1249482132@qq.com>
2020-05-29 09:46:07 +0800

    springcloud首次创建

---
+++ eruaka-client/src/test/java/com/naztuo/eruakaclient/EruakaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naztuo.eruakaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruakaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
