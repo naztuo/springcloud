488e0feffb56352b55e9afa529b3c5f8dcae0f6c
naztuo <1249482132@qq.com>
2020-05-29 11:57:18 +0800

    springcloud feign

--- feign-client/src/main/java/com/naztuo/feignclient/service/SchedualServiceHi.java
+++ feign-client/src/main/java/com/naztuo/feignclient/service/SchedualServiceHi.java
@@ -1,11 +1,12 @@
 package com.naztuo.feignclient.service;
 
+import com.naztuo.feignclient.compent.SchedualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
 
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
